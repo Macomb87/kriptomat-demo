c266dd3c48eb35ab376a7837ec44ef7e19d0c38c
Marko Obradovic <marko.obradovic@bearingpoint.com>
2022-07-12 03:22:50 +0200

    - changed back ( deleted usage of wrong component )

--- src/components/dailyChangeRow.vue
+++ src/components/dailyChangeRow.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="daily-change">
-    <v-img v-if="bearMarket === true" class="arrow-image"
+    <v-img v-if="bearMarket == true" class="arrow-image"
            max-height="12" max-width="12" src="../assets/small_arrow_down.png"></v-img>
-    <v-img v-if="bullMarket === true" class="arrow-image"
-           max-height="12" max-width="12" src="../assets/small_arrow_up.png"></v-img>
+    <v-img v-if="bullMarket == true" class="arrow-image"
+           max-height="12" max-width="12" src="'../assets/small_arrow_up.png"></v-img>
     <p :class="{'green-text': bullMarket,'red-text':bearMarket }">
       {{ parseFloat(price_change_percentage).toFixed(2) }}</p>
   </div>
